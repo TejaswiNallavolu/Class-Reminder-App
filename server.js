d6a6fbaf83c566720920262c704366706fa9b175
Tejaswi Nallavolu <tejaswireddynallavolu@gmail.com>
2022-02-17 23:26:12 -0600

    .

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/MyTrip'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Class-Reminder-App/'}),
+    res.sendFile('index.html', {root: 'dist/MyTrip/'}),
 );
 
 // Start the app by listening on the default Heroku port
